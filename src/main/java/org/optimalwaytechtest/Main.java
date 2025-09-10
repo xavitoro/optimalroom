a0f091b5e020c6b6c64c8d431e01367b5ecd13b7
Xavi Toro <xavitoro@abrots.com>
2025-09-10 12:19:51 +0200

    first commit with base dependencies

---
+++ src/main/java/org/optimalwaytechtest/Main.java
@@ -0,0 +1,9 @@
+package org.optimalwaytechtest;
+
+import org.springframework.boot.SpringApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingsApplication.class, args);
+    }
+}
---
+++ src/main/java/org/optimalwaytechtest/MeetingsApplication.java
@@ -0,0 +1,11 @@
+package org.optimalwaytechtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingsApplication.class, args);
+    }
+}
